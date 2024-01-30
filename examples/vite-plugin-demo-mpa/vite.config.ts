bac6a9a7dbd6d5ff1f2583303fd4e75a3719cc1a
wfly <953212389@qq.com>
2024-01-30 15:34:29 +0800

    fix: 修复 tags 不生效的问题

--- examples/vite-plugin-demo-mpa/vite.config.ts
+++ examples/vite-plugin-demo-mpa/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import htmlTemplate from '../../src';
-import mpa from '../../../vite-plugin-multi-pages';
+import mpa from 'vite-plugin-multi-pages';
 
 export default defineConfig({
   plugins: [
